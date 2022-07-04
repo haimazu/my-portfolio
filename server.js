78915dc8a7809f3eac44294298d622ab8d894840
חיים אזולאי <haim.azulay@e.braude.ac.il>
2022-07-05 00:14:01 +0300

    test

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require('path');
 
 const app = express();
 app.use(express.json());
@@ -25,6 +26,16 @@ mongoose.connect(
     console.log("Connected to MongoDB");
   }
 );
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
