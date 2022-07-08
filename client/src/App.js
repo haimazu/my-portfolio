d0e951c181eaf930b32b7355014be75cd95e4d93
חיים אזולאי <haim.azulay@e.braude.ac.il>
2022-07-08 17:45:27 +0300

    organize pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,30 @@
 import React from "react";
 
-import Home from "./pages/Home";
-import Login from "./pages/Login";
+import { DataProvider } from "../GlobalState";
 
-import { Routes, Route } from "react-router-dom";
+import Header from "../components/Header/Header";
+import Hero from "../components/Hero/Hero";
+import About from "../components/About/About";
+import Companies from "../components/Companies/Companies";
+import Skills from "../components/Skills/Skills";
+import Projects from "../components/Projects/Projects";
+import Contact from "../components/Contact/Contact";
+import Footer from "../components/Footer/Footer";
+import BackTopBtn from "../components/BackTopBtn";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} exact />
-      <Route path="/login" element={<Login />} exact />
-    </Routes>
+    <DataProvider>
+      <Header />
+      <Hero />
+      <About />
+      <Companies />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
+      <BackTopBtn />
+    </DataProvider>
   );
 };
 
--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const path = require('path');
+const path = require("path");
 
 const app = express();
 app.use(express.json());
